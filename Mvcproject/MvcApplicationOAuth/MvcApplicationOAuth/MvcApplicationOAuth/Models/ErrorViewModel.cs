5f11fc37740fba7ec527a61abebf7d48020dafe1
lalit holkar <lalitholkar@gmail.com>
2025-11-02 23:07:22 +0530

    Initial commit

---
+++ Mvcproject/MvcApplicationOAuth/MvcApplicationOAuth/MvcApplicationOAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcApplicationOAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
